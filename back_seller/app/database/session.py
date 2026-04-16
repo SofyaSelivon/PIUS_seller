d87addfc3d6471a55dad3f5fd5057c1645ea7d82
Sofya <si.selivon@yandex.ru>
2026-04-16 16:47:11 +0300

    test hooks

--- back_seller/app/database/session.py
+++ back_seller/app/database/session.py
@@ -4,17 +4,13 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+asyncpg://postgres:postgres@postgres:5432/marketplace_db"
+    "DATABASE_URL", "postgresql+asyncpg://postgres:pass@postgres:5432/marketplace_db"
 )
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-AsyncSessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False
-)
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
 
 async def get_db():
     async with AsyncSessionLocal() as db:
